e37ff04f8e008f27b0c3261552619661067eba28
Daniel Heredia <rorhedia@gmail.com>
2023-05-05 10:57:31 -0600

    Actualización del componente para vista de detalle, cambio a componente genérico

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -5,12 +5,10 @@ import {CharactersComponent} from "./routes/characters/characters.component";
 import {DetailComponent} from "./routes/detail/detail.component";
 
 const routes: Routes = [
-  {
-    path: 'comics',
-    component: ComicsComponent
-  },
+  {path: 'comics', component: ComicsComponent},
+  {path: "comic/:id", component: DetailComponent},
   {path: 'personajes', component: CharactersComponent},
-  {path: "detail/:id", component: DetailComponent},
+  {path: "personaje/:id", component: DetailComponent},
   {path: "**", redirectTo: "/"}
 ];
 
--- src/app/routes/detail/detail.component.ts
+++ src/app/routes/detail/detail.component.ts
@@ -1,8 +1,13 @@
 import {Component, OnInit} from '@angular/core';
 import {ActivatedRoute} from '@angular/router';
 
-import {Comic} from '@interfaces/comics.interface'
 import {ComicsService} from "@services/comics.service";
+import {CharactersService} from "@services/characters.service";
+
+enum pages {
+  comic = 'comic',
+  personaje = 'personaje'
+}
 
 @Component({
   selector: 'app-detail',
@@ -11,32 +16,58 @@ import {ComicsService} from "@services/comics.service";
 })
 export class DetailComponent implements OnInit {
 
-  data: Comic | any = {};
+  data: any = {};
   buyMessage: string = '';
-  comic = {
+  showPrice: boolean = true;
+  spinner: boolean = true;
+  item = {
     title: '',
     description: '',
     image: ''
   }
 
-
   constructor(
     private route: ActivatedRoute,
-    private comicSvc: ComicsService
+    private comicSvc: ComicsService,
+    private characterSvc: CharactersService
   ) {
   }
 
   ngOnInit() {
-    const id = this.route.snapshot.paramMap.get('id') || '0';
-    this.comicSvc.getById(parseInt(id)).subscribe(data => {
+    const urlInfo = this.route.snapshot.url.map(res => res);
+    const section: string = urlInfo[0].path;
+    const item: number = parseInt(urlInfo[1].path);
+
+    if (section === pages.comic) this.getComic(item);
+    else if (section === pages.personaje) this.getCharacter(item);
+
+  }
+
+  getComic(id: number) {
+    this.comicSvc.getById(id).subscribe(data => {
       // @ts-ignore
       this.data = data[0];
       this.buyMessage = `$ ${this.data.prices[0].price}`;
-      this.comic = {
+      this.item = {
         title: this.data.title || '',
         description: this.data.description || null,
         image: `${this.data.images[0].path}.${this.data.images[0].extension}` || '',
       }
+      this.spinner = false;
+    })
+  }
+
+  getCharacter(id: number) {
+    this.showPrice = false;
+    this.characterSvc.getById(id).subscribe(data => {
+      // @ts-ignore
+      this.data = data[0];
+      this.item = {
+        title: this.data.name || '',
+        description: this.data.description || null,
+        image: `${this.data.thumbnail.path}.${this.data.thumbnail.extension}` || '',
+      }
+      this.spinner = false;
     })
   }
 
